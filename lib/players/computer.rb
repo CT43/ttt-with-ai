5c8d7b7a7ea715f4bb37fe976f307c666123eec3
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-10 19:46:56 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -25,39 +25,39 @@ module Players
 
 
     def close(board)
-   Game::WIN_COMBINATIONS.each do |combo|
-     #binding.pry
-     if board.cells[combo[0]] == self.token || board.cells[combo[1]] == self.token
-       if board.cells[combo[0]] == board.cells[combo[1]]
-          @move = combo[2] unless board.taken?(combo[2]+1)
-          break if @move == combo[2]
-       elsif board.cells[combo[1]] == board.cells[combo[2]]
-          @move = combo[0] unless board.taken?(combo[0]+1)
-          break if @move == combo[2]
-       elsif board.cells[combo[2]] == board.cells[combo[0]]
-          @move = combo[1] unless board.taken?(combo[1]+1)
-          break if @move == combo[2]
-       end
-     elsif board.cells[combo[0]] != " " || board.cells[combo[1]] != " "
-       if board.cells[combo[0]] == board.cells[combo[1]]
-         @move = combo[2] unless board.taken?(combo[2]+1)
-       elsif board.cells[combo[1]] == board.cells[combo[2]]
-         @move = combo[0] unless board.taken?(combo[0]+1)
-       elsif board.cells[combo[2]] == board.cells[combo[0]]
-         @move = combo[1] unless board.taken?(combo[1]+1)
+     Game::WIN_COMBINATIONS.each do |combo|
+       #binding.pry
+       if board.cells[combo[0]] == self.token || board.cells[combo[1]] == self.token
+         if board.cells[combo[0]] == board.cells[combo[1]]
+            @move = combo[2] unless board.taken?(combo[2]+1)
+            break if @move == combo[2]
+         elsif board.cells[combo[1]] == board.cells[combo[2]]
+            @move = combo[0] unless board.taken?(combo[0]+1)
+            break if @move == combo[2]
+         elsif board.cells[combo[2]] == board.cells[combo[0]]
+            @move = combo[1] unless board.taken?(combo[1]+1)
+            break if @move == combo[2]
+         end
+       elsif board.cells[combo[0]] != " " || board.cells[combo[1]] != " "
+         if board.cells[combo[0]] == board.cells[combo[1]]
+           @move = combo[2] unless board.taken?(combo[2]+1)
+         elsif board.cells[combo[1]] == board.cells[combo[2]]
+           @move = combo[0] unless board.taken?(combo[0]+1)
+         elsif board.cells[combo[2]] == board.cells[combo[0]]
+           @move = combo[1] unless board.taken?(combo[1]+1)
+         end
        end
      end
+     move_index = @move.to_i + 1
+     move_index.to_s
    end
-   move_index = @move.to_i + 1
-   move_index.to_s
- end
 
- def valid(board)
-   moves = ["1", "9", "3", "7", "5", "6", "4", "2", "8"]
-   moves.detect do |move|
-   board.valid_move?(move) == true
- end
- end
+   def valid(board)
+     moves = ["1", "9", "3", "7", "5", "6", "4", "2", "8"]
+     moves.detect do |move|
+     board.valid_move?(move) == true
+    end
+   end
 
     def move(board)
       if board.turn_count == 0
@@ -79,6 +79,5 @@ module Players
        end
       end
     end
-
   end
 end

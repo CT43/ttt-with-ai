2c306c0861120794b9239c18ad9785c1a9e76645
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-10 18:16:09 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -13,8 +13,8 @@ module Players
      #binding.pry
      if board.cells[combo[0]] == self.token || board.cells[combo[1]] == self.token
        if board.cells[combo[0]] == board.cells[combo[1]]
-         @move = combo[2] unless board.taken?(combo[2]+1)
-         break
+         return @move = combo[2] unless board.taken?(combo[2]+1)
+
        elsif board.cells[combo[1]] == board.cells[combo[2]]
          @move = combo[0] unless board.taken?(combo[0]+1)
          break

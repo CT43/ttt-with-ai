3668d4f4a301ea449b925b7613915e3e66d707a3
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-10 19:05:01 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -15,9 +15,9 @@ module Players
        if board.cells[combo[0]] == board.cells[combo[1]]
           @move = combo[2] unless board.taken?(combo[2]+1)
        elsif board.cells[combo[1]] == board.cells[combo[2]]
-         return @move = combo[0] unless board.taken?(combo[0]+1)
+          @move = combo[0] unless board.taken?(combo[0]+1)
        elsif board.cells[combo[2]] == board.cells[combo[0]]
-         return @move = combo[1] unless board.taken?(combo[1]+1)
+          @move = combo[1] unless board.taken?(combo[1]+1)
        end
      elsif board.cells[combo[0]] != " " || board.cells[combo[1]] != " "
        if board.cells[combo[0]] == board.cells[combo[1]]

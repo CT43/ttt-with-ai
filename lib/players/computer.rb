bfd6d41c14f2aa90480f0ac943a4b39ef2d1f0ca
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-10 19:46:27 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,12 +1,7 @@
 module Players
   class Computer < Player
     attr_accessor :move
-    #def move(board)
-    #  moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    #  moves.detect do |move|
-    #    board.valid_move?(move) == true
-    #  end
-    #end
+    
     def move(board)
       if board.turn_count == 0
         "1"
@@ -69,7 +64,7 @@ module Players
         "1"
       elsif board.turn_count == 1 && !board.taken?(5)
         "5"
-      elsif (board.turn_count == 3 and self.token == board.cells[4]) and ((board.cells[0] != " " and board.cells[8] != " ") || (board.cells[2] != " " and board.cells[6] != " "))
+      elsif (board.turn_count == 3 && self.token == board.cells[4]) && ((board.cells[0] != " " && board.cells[8] != " ") || (board.cells[2] != " " && board.cells[6] != " "))
         if board.cells[1] == " "
           "2"
         else
